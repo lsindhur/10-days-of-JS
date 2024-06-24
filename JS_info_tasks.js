f8b1a0ae9ddc88d90d254a894f4702aacfbb63f9
lsindhu <sindhu151095@gmail.com>
2024-06-24 17:35:19 +0530

    JavaScript.Info tasks 24/6

---
+++ JS_info_tasks.js
@@ -0,0 +1,13 @@
+let admin;
+let personName = "John";
+admin = personName;
+console.log(admin);
+
+// naming variables right
+
+let Earth; //variable named after our planet
+let visitor; //variable for visitor to the website
+
+
+
+
